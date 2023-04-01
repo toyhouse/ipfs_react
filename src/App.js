f181aea07c281c4b25fdd2f55cd8de150bffbadc
Ben Koo <koo0905@gmail.com>
2023-04-01 10:36:11 +0800

    Added ThirdWeb lib

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Hello World with IPFS! Version 2</h1>
+        <h1>Hello World with IPFS! with ThirdWeb Lib</h1>
     </div>
   );
 }
