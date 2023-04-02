706d353fec4dc48f4f286a6a805affe1c2d5bf8e
Ben Koo <koo0905@gmail.com>
2023-04-02 08:57:08 +0800

    changed Apps.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Hello World with IPFS! with ThirdWeb and CICD</h1>
+        <h1>Hello World with IPFS! with James and CICD on April 2, 2023</h1>
     </div>
   );
 }
